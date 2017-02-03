882af27979f3c4881c5f141bcf964bbd2afe556f
Jinzhe Li <jinzhe@simple.com.au>
2017-02-03 15:57:49 +1030

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Simple Validate jQuery Plugin
+
+## Dependency
+
+- [jQuery](https://code.jquery.com/jquery-3.1.0.js)
+- [Tether](https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js)
+Both are not included in this plugin.
+
+## Usage
+
+call on form element so that submit event is binded.
+```
+$("form").simplevalidate()
+```
+Then add rules to the validate plugin by 'addRules' or 'setRules' method (see below).
+
+A single rule is an object of the following format:
+```
+{
+	target: "#target",
+	template: "#template",
+	condition: function() {
+		
+	}
+}
+```
+additionally, it can have some extra options which will be passed into Tether. For example:
+```
+{
+	target: "#target",
+	template: "#template",
+	condition: function() {
+		
+	},
+	classPrefix: 'validation-tether',
+    attachment: "bottom right",
+    targetAttachment: "top right",
+    offset: "5px 0",
+}
+```
+You could check the api documentation for Tether [here](http://tether.io/)
+
+A template is the HTML template element `<template>xxx</template>`
+
+## Method
+
+Belows are list of methods you can call to interact with the plugin
+
+- `$("form").simplevalidate('setRules',[rule1,rule2...])`
+- `$("form").simplevalidate('getRules')`
+- `$("form").simplevalidate('addRules',[rule1,rule2...])`
+- `$("form").simplevalidate('validate')`
+- `$("form").simplevalidate('errors')`
---
+++ dist/simplevalidate.js
@@ -0,0 +1,137 @@
+(function($) {
+
+
+    var methods = {
+        init: function() {
+            if (!this.is("form")) {
+                $.error("You have to init simplevalidate on the form element");
+            } else if (Tether === undefined) {
+                $.error("Tether is not loaded.");
+            } else {
+                this.off('submit', _internal.onSubmitEvent)
+                this.on('submit', _internal.onSubmitEvent);
+            }
+            return this;
+        },
+        setRules: function(rules) {
+            _internal.getRules.setRules.call(this, rules);
+            return this;
+        },
+        getRules: function() {
+            return _internal.getRules.apply(this);
+        },
+        addRules: function(rules) {
+            var existingRules = _internal.getRules.apply(this);
+            var newRules;
+            if (Array.isArray(rules)) {
+                newRules = existingRules.concat(rules);
+            } else {
+                existingRules.push(rules);
+                newRules = existingRules;
+            }
+            _internal.setRules.call(this, newRules);
+            return this;
+        },
+        validate: function() {
+            var rules = _internal.getRules.apply(this);
+            var hasErrors = false;
+            for (var i = 0; i < rules.length; i++) {
+                var rule = rules[i];
+                if (!rule.condition.apply(this)) {
+                    hasErrors = true;
+                }
+            }
+            return !hasErrors;
+        },
+        errors: function() {
+            var rules = _internal.getRules.apply(this);
+            var defaultOptions = _internal.getDefaultOption();
+            var hasErrors = false;
+            var errorElems = [];
+            for (var i = 0; i < rules.length; i++) {
+                var rule = rules[i];
+                if (!rule.condition.apply(this)) {
+                    var elementContent = $(rule.template).html();
+                    var element = $(elementContent);
+                    $("body").append(element);
+                    var target = $(rule.target);
+                    var tether = new Tether($.extend(defaultOptions, {
+                        target: target,
+                        element: element
+                    }, rule));
+                    tether.position();
+                    errorElems.push({
+                        elem: element,
+                        tether: tether
+                    });
+                }
+
+            }
+            return errorElems;
+        }
+    };
+
+    var _internal = {
+        getDefaultOption: function() {
+            return {
+                classPrefix: 'validation-tether',
+                attachment: "bottom right",
+                targetAttachment: "top right",
+                offset: "5px 0"
+            }
+        },
+        onSubmitEvent: function(e) {
+            if (!$(this).simplevalidate('validate')) {
+                e.preventDefault();
+                console.log("You've got error in your form");
+                var errorElems = $(this).simplevalidate('errors');
+                if (errorElems.length) {
+                    e.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: $(errorElems[0].elem).offset().top
+                    }, 500);
+                }
+            } else {
+                console.log("All good")
+            }
+        },
+        setRules: function(rules) {
+            if (Array.isArray(rules)) {
+                this.data('rules', rules);
+            } else {
+                $.error("rules has to be an array");
+            }
+        },
+        getRules: function() {
+            return this.data('rules') ? this.data('rules') : [];
+        },
+    }
+
+
+
+    $.fn.simplevalidate = function(methodOrOptions) {
+        if (methods[methodOrOptions]) {
+            return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            // Default to "init"
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + methodOrOptions + ' does not exist on jQuery.simplevalidate');
+        }
+    };
+
+
+
+})(jQuery);
+
+var foo = $('#form').simplevalidate()
+    .simplevalidate('addRules', {
+        condition: function() {
+            return false;
+        },
+        target: "#error-target",
+        template: "#my-template"
+    })
+    .simplevalidate('getRules');
+
+console.log(foo);
